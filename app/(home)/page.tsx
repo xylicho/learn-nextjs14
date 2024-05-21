dd874d7e8797c91e422ac7013efd7af9b2446a38
xylicho <xylicho15@gmail.com>
2024-05-21 10:56:02 +0900

    deployment

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,32 +1,32 @@
-import Movie from "../../components/movie";
+// import Movie from "../../components/movie";
 import styles from "../../styles/home.module.css";
-import {API_URL} from '../constans';
+import { API_URL } from "../constans";
 
 export const metadata = {
   title: "Home",
 };
 
-
-
 export async function getMovies() {
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
-  const response = await fetch(API_URL);
-  const json = await response.json();
-  return json;
+  // const response = await fetch(API_URL);
+  // const json = await response.json();
+  // return json;
+  return 1;
 }
 
 export default async function HomePage() {
   const movies = await getMovies();
+  console.log(movies)
   return (
+    // <p>d</p>
     <div className={styles.container}>
-      {movies.map((movie) => (
+      {/* {movies.map((movie) => (
         <Movie
           key={movie.id}
           id={movie.id}
           poster_path={movie.poster_path}
           title={movie.title}
         />
-      ))}
+      ))} */}
     </div>
   );
 }
